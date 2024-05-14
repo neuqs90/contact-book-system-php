90f58f042721fee8bcb3e93aa24f767a7421b186
neuqs90 <e404user@gmail.com>
2024-05-14 19:50:09 +0530

    Renamed To .php for php code and removed css in different file

---
+++ signup.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup Form</title>
+    <link rel="stylesheet" href="STYLES/signup.css">
+</head>
+
+<body>
+    <h1>Signup Page</h1>
+    <div class="signup">
+        <form id="signup" method="get" action="login.php">
+            <label for="Uname"><b>User Name</b></label>
+            <input type="text" name="Uname" id="Uname" placeholder="Username">
+
+            <label for="emai"><b>Email</b></label>
+            <input type="email" name="email" id="email" placeholder="email">
+
+            <label for="Pass"><b>Password</b></label>
+            <input type="password" name="Pass" id="Pass" placeholder="Password">
+
+            <input type="button" name="sign" id="sign" value="Signup">
+            
+        </form><br>
+        <button class="login-btn" onclick="location.href='';">Already have an account? Login</button>
+    </div>
+</body>
+
+</html>
