75d50dd0d9bfdd7c10df9952f8e7f87bff331a65
neuqs90 <e404user@gmail.com>
2024-05-19 07:53:16 +0530

    dk

--- signup.php
+++ signup.php
@@ -1,3 +1,18 @@
+<?php
+
+if isset($_POST["sign"]){
+
+    $username = trim($_POST["Uname"])
+    $password = trim($_POST["Pass"])
+    $email = trim($_POST["email"])
+
+    
+
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,13 +28,13 @@
     <div class="signup">
         <form id="signup" method="get" action="login.php">
             <label for="Uname"><b>User Name</b></label>
-            <input type="text" name="Uname" id="Uname" placeholder="Username">
+            <input type="text" name="Uname" id="Uname" placeholder="Username" required>
 
             <label for="emai"><b>Email</b></label>
-            <input type="email" name="email" id="email" placeholder="email">
+            <input type="email" name="email" id="email" placeholder="email" required>
 
             <label for="Pass"><b>Password</b></label>
-            <input type="password" name="Pass" id="Pass" placeholder="Password">
+            <input type="password" name="Pass" id="Pass" placeholder="Password" required>
 
             <input type="button" name="sign" id="sign" value="Signup">
             
